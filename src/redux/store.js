18a75fa1cf44edb45a87d9fdd72189f7155886f3
Kateryna-Urbanovych <katya_urb@ukr.net>
2021-01-24 01:07:09 +0200

    удаляет redux-persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,44 +1,18 @@
 import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import logger from 'redux-logger';
-import {
-    persistStore,
-    persistReducer,
-    FLUSH,
-    REHYDRATE,
-    PAUSE,
-    PERSIST,
-    PURGE,
-    REGISTER,
-} from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
 import contactsReducer from './contacts/contacts-reducer';
 
-const contactsPersistConfig = {
-    key: 'rootReducer',
-    storage,
-    blacklist: ['filter'],
-};
-
-const middleware = [
-    ...getDefaultMiddleware({
-        serializableCheck: {
-            ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-        },
-    }),
-    logger,
-];
+const middleware = [...getDefaultMiddleware(), logger];
 
 const store = configureStore({
     reducer: {
-        contacts: persistReducer(contactsPersistConfig, contactsReducer),
+        contacts: contactsReducer,
     },
     middleware,
     devTools: process.env.NODE_ENV === 'development',
 });
 
-const persistor = persistStore(store);
-
-export { store, persistor };
+export default store;
 
 // пример state
 // {
